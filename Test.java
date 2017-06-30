74e3cfcfd0f798e94a2a7dbc9a649c4ea9436322
yasser <yasser.kfupm@hotmail.com>
2017-06-30 07:19:38 +0300

    add numbers :)

---
+++ Test.java
@@ -0,0 +1,16 @@
+package tweet;
+import static java.lang.System.*;
+
+import java.util.regex.*;
+
+public class Test{
+	
+	public static void main(String [] arg){
+		
+		out.print("The ruslt is : ");
+		out.print(1333+222l);
+	}
+	
+}
+
+
